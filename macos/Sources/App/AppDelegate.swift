4823d34800ebbe3ac5c4413001a4aa282ccde32a
전영호 <youngho.jeon@datamaker.io>
2026-02-24 17:20:15 +0900

    feat(macos): scaffold Swift menu bar app with NSStatusItem

---
+++ macos/Sources/App/AppDelegate.swift
@@ -0,0 +1,22 @@
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        if let button = statusItem.button {
+            button.image = NSImage(
+                systemSymbolName: "mic.slash",
+                accessibilityDescription: "PTT Dictation"
+            )
+        }
+
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "No devices connected", action: nil, keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
+    }
+}
